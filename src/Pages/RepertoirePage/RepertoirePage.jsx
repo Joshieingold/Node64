4206818a9ddb82a65a816c1d8b341a7f7c0617d2
joshieingold <joshlynch2001@gmail.com>
2026-07-10 16:32:23 -0300

    can now switch between different views, at the cost of the normal analysis notation needing fixing

--- src/Pages/RepertoirePage/RepertoirePage.jsx
+++ src/Pages/RepertoirePage/RepertoirePage.jsx
@@ -2,9 +2,11 @@ import "./RepertoirePage.css";
 import { useState, useEffect } from "react";
 import ChessBoard from "../../Components/Board/Board";
 import RepertoireGraph from "../../Components/RepertoireGraph/RepertoireGraph";
+import Notation from "../../Components/Notation/Notation";
 
 export default function RepertoirePage({ data }) {
     const [, setVersion] = useState(0);
+    const [currentPanelView, setCurrentPanelView] = useState("NodeView");
     const update = () => {
         setVersion((v) => v + 1);
     };
@@ -22,8 +24,34 @@ export default function RepertoirePage({ data }) {
             <div className="repertoire-board-container">
                 <ChessBoard data={data} update={update} />
             </div>
-            <div className="repertoire-graph-container">
-                <RepertoireGraph data={data} update={update} />
+            <div className="tab-panel-container">
+                <div className="panel-control-wrapper">
+                    <div
+                        className="panel-option"
+                        onClick={() => setCurrentPanelView("NodeView")}
+                    >
+                        Node View
+                    </div>
+                    <div
+                        className="panel-option"
+                        onClick={() => setCurrentPanelView("ScoreSheet")}
+                    >
+                        ScoreSheet View
+                    </div>
+                    <div className="panel-option">Memorize Lines</div>
+                </div>
+                <div className="panel-options-wrapper">
+                    <div
+                        className={`panel-view ${currentPanelView === "NodeView" ? "" : "hidden"}`}
+                    >
+                        <RepertoireGraph data={data} update={update} />
+                    </div>
+                    <div
+                        className={`panel-view ${currentPanelView === "ScoreSheet" ? "" : "hidden"}`}
+                    >
+                        <Notation data={data} />
+                    </div>
+                </div>
             </div>
         </div>
     );
