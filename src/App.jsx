0e281a95fb48fbae8c703b8882aa9df9101f83f9
joshieingold <joshlynch2001@gmail.com>
2026-07-22 19:38:53 -0300

    maybe ill just use the lichess board and improve my backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { Chessground } from "chessground";
 import "./App.css";
 import Shell from "./NEW/Shell/Shell";
+import { ChessgroundBoard } from "./ChessGround";
+import { GameBoard } from "./GameBoard";
 
 export default function App() {
-    return <Shell />;
+    return <GameBoard />;
 }
